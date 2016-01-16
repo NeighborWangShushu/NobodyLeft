43f5ce3776bd1a232c5164656ecf9dca49b19fa1
Ik ku dar anai <shvier@shvierdemacbook-pro.local>
2016-01-16 14:54:23 +0800

    初始化工程

---
+++ Demo/Demo/Classes/AppDelegate.h
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.h
+//
+//  Created by : Shvier
+//  Project    : Demo
+//  Date       : 16/1/16
+//
+//  Copyright (c) 2016年 boom.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------------
+
+@interface AppDelegate : CCAppDelegate
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
